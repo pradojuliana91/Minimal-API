c1039701fac3b9ffe2e087d966d99bd64be1084d
Juliana Prado <ju.pradoo@gmail.com>
2024-10-02 15:16:56 -0300

    Iniciando projeto

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApi.Dominio.DTOs
+{
+    public class LoginDTO
+    {
+
+        public string Email { get; set; } = default!;
+        public string Senha { get; set; } = default!;
+    }
+}
